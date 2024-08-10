e06bfaa2790e4635fed94c2f718424d4eb7f6500
ArturPososhko <44324534+ArturPososhko@users.noreply.github.com>
2024-08-10 11:50:53 +0000

    Updated config template

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 config = {
-    "google_api_key": "SECRET",
-    "youtube_playlist_id": "PLr3-bEi1N4khKZm2u6Q8pOLO-WNZrKDHn",
+    "google_api_key": "<your key>",
+    "youtube_playlist_id": "<your playlist ID>",
+    "kafka": {
+        "bootstrap.servers": "<your serve>",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "<your key>",
+        "sasl.password": "<your secret>",
+    },
+    "schema_registry": {
+        "url": "<your SR server>",
+        "basic.auth.user.info": "<your SR key>:<your SR secret>",
+    }
 }
